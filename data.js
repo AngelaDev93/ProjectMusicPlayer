58f69c4651bbafe28282cc5c0e5eee912534cc52
Angela Fandiño <angiela2332@gmail.com>
2021-12-06 20:50:41 -0500

    Agregue carpeta CSS, js, index.html, imagenes y música

---
+++ data.js
@@ -0,0 +1,62 @@
+let songs = [
+    {
+        name: 'The Motto',
+        path: 'Accesos/Musica/SongElectronica.mp3',
+        artist: 'Tiesto & Ava Max',
+        cover: 'Accesos/Imagenes/img5.jpeg'
+    },
+    {
+        name: 'Bailar Contigo',
+        path: 'Accesos/Musica/songMonsieur.m4a',
+        artist: 'Monsieur Perine',
+        cover: 'Accesos/Imagenes/img12.jpeg'
+    },
+    {
+        name: 'Saca,prende y sorprende',
+        path: 'Accesos/Musica/songReggae.mp3',
+        artist: 'Cultura Profetica',
+        cover: 'Accesos/Imagenes/img10.jpeg'
+    },
+    {
+        name: 'Toxicity',
+        path: 'Accesos/Musica/songsystem.mp3',
+        artist: 'System Of a Down',
+        cover: 'Accesos/Imagenes/img13.jpeg'
+    },
+    {
+        name: 'Serenidad',
+        path: 'Accesos/Musica/songmusicaclasica.mp3',
+        artist: 'Yoga,Meditacion',
+        cover: 'Accesos/Imagenes/img6.jpeg'
+    },
+    {
+        name: 'Levitating',
+        path: 'Accesos/Musica/songdualipa.flac',
+        artist: 'Dua Lipa',
+        cover: 'Accesos/Imagenes/img9.jpeg'
+    },
+    {
+        name: 'Umbrella',
+        path: 'Accesos/Musica/songrihanna.mp3',
+        artist: 'Rihanna',
+        cover: 'Accesos/Imagenes/img8.jpeg'
+    },
+    {
+        name: 'Cóseme',
+        path: 'Accesos/Musica/songjuliana.m4a',
+        artist: 'Juliana Velasquez',
+        cover: 'Accesos/Imagenes/img7.jpeg'
+    },
+    {
+        name: 'París',
+        path: 'Accesos/Musica/songparis.mp3',
+        artist: 'Ingratax',
+        cover: 'Accesos/Imagenes/img2.jpeg'
+    },
+    {
+        name: 'Carry on Wayward Son',
+        path: 'Accesos/Musica/songkansas.mp3',
+        artist: 'Kansas',
+        cover: 'Accesos/Imagenes/img3.jpeg'
+    },
+]
