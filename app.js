4b558423db8bdfe5459fb0a4a4478fb2c3bfcb48
Angela Fandiño <angiela2332@gmail.com>
2021-12-14 22:30:23 -0500

    incluided buttons and seekbar events

--- app.js
+++ app.js
@@ -1,5 +1,3 @@
-/////carousels///////////
-
 const gallery = [...document.querySelectorAll('.gallery img')];
 
 let  carouselImageIndex = 0;
@@ -19,9 +17,7 @@ setInterval(() => {
     changeCarousel();
 }, 3000);
 
-///////navigation//////////
-
-//////////toggling music player////////
+/////Section music player//////
 
 const sectionMusicPlayer = document.querySelector('.section-music-player');
 
@@ -39,7 +35,7 @@ sectionMusicPlayer.addEventListener('click', () => {
     }, 250);
 })
 
-///////back from music player//////
+/////navigation init////////////
 
 const backToHomeBtn = document.querySelector('.section-music-player .back-button');
 
@@ -47,7 +43,6 @@ backToHomeBtn.addEventListener('click', () => {
     sectionMusicPlayer.classList.remove('active');
 })
 
-///////playlist////////////
 
 const playlistSection = document.querySelector('.playlist');
 const navBtn = document.querySelector('.section-music-player .nav-button');
@@ -63,3 +58,153 @@ backToMusicPlayer.addEventListener('click', () => {
 })
 
 
+/////////finish navigation///////
+
+//////music//////
+
+let CurrentMusic = 0;
+
+const music = document.querySelector('#audio-source');
+
+const seekBar = document.querySelector('.music-seek-bar');
+const SongName = document.querySelector('.current-song-name');
+const artistName =document.querySelector('.artist-name');
+const coverImage = document.querySelector('.cover');
+const currentMusicTime = document.querySelector('.current-time');
+const musicDuration = document.querySelector('.duration');
+
+const list = [...document.querySelectorAll('.list')];
+
+////selector buttons//////
+
+const forwardBtn = document.querySelector('i.fa-forward');
+const backwardBtn = document.querySelector('i.fa-backward');
+const playBtn = document.querySelector('i.fa-play');
+const pauseBtn = document.querySelector('i.fa-pause');
+const repeatBtn = document.querySelector('span.fa-redo');
+const volumeBtn = document.querySelector('span.fa-volume-up');
+const volumeSlider = document.querySelector('.volume-slider');
+
+/////funtion for setting up music//////
+
+const setMusic = (i) => {
+    seekBar.value = 0;
+    let song = songs[i];
+    CurrentMusic = i;
+
+    music.src = song.path;
+
+    SongName.innerHTML = song.name;
+    artistName.innerHTML = song.artist;
+    coverImage.src = song.cover;
+
+    setTimeout(() => {
+       seekBar.max = music.duration;
+       musicDuration.innerHTML = formatTime(music.duration);
+    }, 300);
+    currentMusicTime.innerHTML = '00 : 00';
+    list.forEach(item => item.classList.remove('active'));
+    list[CurrentMusic].classList.add('active');
+
+}
+
+setMusic(0);
+
+////format duration in 00 : 00 format////
+
+const formatTime = (time) => {
+    let min = Math.floor(time / 60);
+    if(min < 10){
+        min = `0` + min;
+    }
+
+    let sec = Math.floor(time % 60);
+    if(sec < 10){
+        sec = `0` + sec;
+    }
+
+    return `${min} : ${sec}`;
+}
+
+////playBtn click event/////
+
+playBtn.addEventListener('click', () => {
+    music.play();
+    playBtn.classList.remove('active');
+    pauseBtn.classList.add('active');
+})
+
+////pauseBtn click event/////
+
+pauseBtn.addEventListener('click', () => {
+    music.pause();
+    pauseBtn.classList.remove('active');
+    playBtn.classList.add('active');
+})
+
+////forward button//////
+
+forwardBtn.addEventListener('click', () => {
+    if(CurrentMusic >= songs.length - 1){
+        CurrentMusic = 0;
+    } else{
+        CurrentMusic++;
+    }
+    setMusic(CurrentMusic);
+    playBtn.click();
+})
+
+///////backward button/////
+
+backwardBtn.addEventListener('click', () => {
+    if(CurrentMusic <= 0){
+        CurrentMusic = songs.length - 1;
+    } else{
+        CurrentMusic--;
+    }
+    setMusic(CurrentMusic);
+    playBtn.click();
+})
+
+///seekbar events/////
+
+setInterval(() => {
+    seekBar.value = music.currentTime;
+    currentMusicTime.innerHTML = formatTime(music.currentTime);
+    if(Math.floor(music.currentTime) == Math.floor(seekBar.max)){
+        if(repeatBtn.className.includes('active')){
+            setMusic(CurrentMusic);
+            playBtn.click();
+        } else{
+            forwardBtn.click();
+        }
+    }
+}, 500)
+
+seekBar.addEventListener('change', () => {
+    music.currentTime = seekBar.value;
+})
+
+//////repeat button/////
+
+repeatBtn.addEventListener('click', () => {
+    repeatBtn.classList.toggle('active');
+})
+
+////volume////////
+
+volumeBtn.addEventListener('click', () => {
+    volumeBtn.classList.toggle('active');
+    volumeSlider.classList.toggle('active');
+})
+
+volumeSlider.addEventListener('input', () => {
+    music.volume = volumeSlider.value;
+})
+
+list.forEach((item, i) =>{
+    item.addEventListener('click', () => {
+        setMusic(i);
+        playBtn.click();
+    })
+})
